37ea4b3a0b1c3e710fb16f4f3676b783b339081d
Vincent Vlk <vlkv@vlkv.name>
2022-07-21 17:17:08 +0200

    uloha: len ukladam pripisany text

--- vlan-id-check.py
+++ vlan-id-check.py
@@ -10,14 +10,15 @@ from netmiko import Netmiko
 from getpass import getpass
 import datetime
 
-# nastavime potrebne premenne:
-# nastavujeme premennu "teraz" s aktualnym systemovym casom, kvoli logovaniu
-
+'''
+- nastavime potrebne premenne:
+- nastavujeme premennu "teraz" s aktualnym systemovym casom, kvoli logovaniu
+'''
 f_zoznam = 'flotila.zoznam'
 uzivatel = 'sshview'
 
 vlan_id = input('\nZadaj cislo hladanej VLAN: ')
-heslo = getpass('Zadajte SSH heslo pre uzivatela \"' + uzivatel +'\": ')
+heslo = getpass('Zadajte SSH heslo pre uzivatela \"' + uzivatel + '\": ')
 
 prikaz = 'show vlan id ' + vlan_id + ' | include active|not'
 teraz = str(datetime.datetime.now().strftime('%d.%m.%Y %H:%M:%S'))
@@ -26,29 +27,34 @@ teraz = str(datetime.datetime.now().strftime('%d.%m.%Y %H:%M:%S'))
 try:
     f = open(f_zoznam, 'r')
     b_zoznam = f.readlines()
-except:
+except FileNotFoundError:
     print('\n' + teraz + ' --> CHYBA pri otvarani suboru: ' + f_zoznam + '\n')
 
 print()
-# spustime for slucku, ktora sa na IP/name v zozname pripoji a vykona "prikaz"
-# do premennej "vystup" ulozime odpoved zo zadaneho prikazu z prem. "prikaz"
-for riadok in b_zoznam :
+
+'''
+- spustime for slucku, ktora sa na IP/name v zozname pripoji a vykona "prikaz"
+- do premennej "vystup" ulozime odpoved zo zadaneho prikazu z prem. "prikaz"
+'''
+for riadok in b_zoznam:
     teraz = str(datetime.datetime.now().strftime('%d.%m.%Y %H:%M:%S'))
 
     print(teraz + ' --> Pripajam sa na adresu:', riadok)
     try:
-        pripojenie = Netmiko(host = riadok,
-                             device_type = 'cisco_nxos', 
-                             username = uzivatel, 
-                             password = heslo)
+        pripojenie = Netmiko(host=riadok,
+                             device_type='cisco_nxos',
+                             username=uzivatel,
+                             password=heslo)
 
         vystup = (pripojenie.send_command(prikaz))
 
-    except:
+    except (Netmiko.ssh_exception):
         print(teraz + ' --> CHYBA pri SSH pripojeni na:', riadok)
         vystup = '-NEDOSTUPNY-'
 
     pripojenie.disconnect
 
     print('Odpoved z', riadok[:-1], ':', vystup)
+
+teraz = str(datetime.datetime.now().strftime('%d.%m.%Y %H:%M:%S'))
 print(teraz + ' --> KONIEC skriptu.')
